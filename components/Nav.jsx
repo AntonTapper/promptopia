bd53dacd30d2621400ff1e5b4cd4c26d8802f517
Anton Tapper Dock <at@appsson.se>
2023-09-06 13:39:47 +0200

    sign in with google works

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-    const isUserLoggedIn = true
-
+    const { data: session} = useSession()
     const [providers, setProviders] = useState(null)
     const [toggleDropdown, settoggleDropdown] = useState(false)
 
     useEffect(() => {
-        const setProviders = async ( ) => {
+        const setUpProviders = async ( ) => {
             const response = await getProviders();
 
             setProviders(response);
         }
 
-        setProviders();
+        setUpProviders();
     }, [])
 
   return (
@@ -34,9 +33,10 @@ const Nav = () => {
             <p className='logo_text'>Promptiopa</p>
         </Link>
 
+
         {/* Desktop Navigation */}
         <div className='sm:flex hidden'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex gap-3 md:gap-5'>
                     <Link href="/create-prompt"
                     className='black_btn'>
@@ -47,7 +47,7 @@ const Nav = () => {
                     </button>
                     <Link href="/profile">
                         <Image 
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
@@ -73,10 +73,10 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className='sm:hidden flex relative'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex'>
                     <Image
-                    src="/assets/images/logo.svg"
+                    src={session?.user.image}
                     width={37}
                     height={37}
                     className='rounded-full'
